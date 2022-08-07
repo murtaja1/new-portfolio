aa298332caea4e4f28953cd1dfaf479a445aa99c
murtaja1 <murtajaadnan7@gmail.com>
2022-08-07 20:30:37 +0300

    Refactor: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-	return <div className="App">hello</div>;
+	return <div>hello</div>;
 }
 
 export default App;
