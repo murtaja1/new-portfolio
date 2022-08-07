5b796098c1d67ef9c8881d6b7cc8fb6c14b12e7a
murtaja1 <murtajaadnan7@gmail.com>
2022-08-07 21:19:24 +0300

    Build: complete routing and create not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-
+import Container from "./containers/Container";
 
 function App() {
-	return <div>hello</div>;
+	return <Container />;
 }
 
 export default App;
--- src/containers/Container.tsx
+++ src/containers/Container.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./about/About";
+import Contact from "./contact/Contact";
+import Home from "./home/Home";
+import NotFound from "./notFound/NotFound";
+import Skills from "./skills/Skills";
+import Work from "./work/Work";
 
 function Container() {
-	return <div>Container</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/work" element={<Work />} />
+				<Route path="/skills" element={<Skills />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default Container;
