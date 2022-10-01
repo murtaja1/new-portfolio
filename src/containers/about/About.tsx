a85730f2f21d44d7caf9d3bf1b61de053ac490c9
murtaja1 <murtajaadnan7@gmail.com>
2022-10-01 20:14:26 +0300

    Build: creat about us page and add the icons

--- src/containers/about/About.tsx
+++ src/containers/about/About.tsx
@@ -1,7 +1,71 @@
-import React from "react";
+import { Box, Container, Grid, Typography, useMediaQuery } from "@mui/material";
+import { useState } from "react";
+import { DivGrow, languages } from "./Data";
+const Slide = require("react-reveal/Slide");
 
 function About() {
-	return <div>About</div>;
+	const mobile = useMediaQuery("(min-width:600px)");
+	const [show, setShow] = useState(false);
+
+	return (
+		<Container
+			maxWidth="xl"
+			sx={{ mt: mobile ? "100px" : "10px", mb: mobile ? "10px" : "100px" }}
+		>
+			<Grid container justifyContent="center" alignItems="center" spacing={3}>
+				<Grid item xs={10}>
+					<Typography variant="h2" sx={{ fontWeight: "bold" }}>
+						About Me:
+					</Typography>
+				</Grid>
+				<Grid item xs={5}>
+					<Typography variant="h4">
+						Over the past two years, I have been developing and participating in
+						different kinds of projects with various technologies and languages.
+						One of the projects is a smart attendance web application called
+						Seat Score, the other is a mobile application for delivery and more.
+						As a software developer by training and at heart I invest in my
+						learning to become a better developer and delivery high quality
+						work.
+					</Typography>
+				</Grid>
+				<Grid item container justifyContent="space-between" xs={12}>
+					<Grid
+						item
+						xs={10}
+						sx={{ overflow: "hidden", mt: "15px", height: "500px" }}
+					>
+						{languages.map((icon, i) => (
+							<Box key={i}>
+								<Slide right delay={icon.delay}>
+									<DivGrow
+										style={{
+											animationDelay: icon.sDelay,
+											top: icon.top,
+											left: icon.left,
+										}}
+									>
+										<img src={icon.icon} width="100%" height="100%" />
+									</DivGrow>
+								</Slide>
+							</Box>
+						))}
+					</Grid>
+					<Grid item xs={2}>
+						<img
+							onClick={() => {
+								setShow(!show);
+							}}
+							src="/gun.svg"
+							alt="d"
+							height="100px"
+							style={{ transform: "rotateY(180deg)", cursor: "pointer", marginTop:'20px' }}
+						/>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Container>
+	);
 }
 
 export default About;
---
+++ src/containers/about/Data.ts
@@ -0,0 +1,219 @@
+import django from "../../assets/django.svg";
+import flask from "../../assets/flask.svg";
+import material from "../../assets/material.svg";
+import vueBootstrap from "../../assets/vueBootstrap.svg";
+import git from "../../assets/git.svg";
+import redux from "../../assets/redux.svg";
+import reactBootstrap from "../../assets/reactBootstrap.js";
+import typescript from "../../assets/typescript.svg";
+import postgres from "../../assets/postgres.svg";
+import firebase from "../../assets/firebase.svg";
+import dart from "../../assets/dart.svg";
+import flutter from "../../assets/flutter.svg";
+import database from "../../assets/database.svg";
+import html from "../../assets/html.svg";
+import react from "../../assets/react.svg";
+import vue from "../../assets/vue.svg";
+import python from "../../assets/python.svg";
+import css from "../../assets/css.svg";
+import bootstrap from "../../assets/bootstrap.svg";
+
+import styled from "@emotion/styled";
+
+export const languages = [
+	// first column
+	{
+		icon: typescript,
+		name: "TYPESCRIPT",
+		color: "",
+		delay: 200,
+		sDelay: "0s",
+		top: "10px",
+		left: "0px",
+	},
+	{
+		icon: postgres,
+		name: "POSTGRES",
+		color: "#e34f26",
+		delay: 400,
+		sDelay: "0.2s",
+		top: "110px",
+		left: "0px",
+	},
+	{
+		icon: firebase,
+		name: "Firebase",
+		color: "#0048BA",
+		delay: 800,
+		sDelay: "0.4s",
+		top: "210px",
+		left: "0px",
+	},
+	// second column
+	{
+		icon: redux,
+		name: "REDUX",
+		color: "#00d8ff",
+		delay: 1000,
+		sDelay: "0.6s",
+		top: "10px",
+		left: "130px",
+	},
+	{
+		icon: reactBootstrap,
+		name: "React-bootstrap",
+		color: "#f0db4f",
+		delay: 1200,
+		sDelay: "0.8s",
+		top: "110px",
+		left: "130px",
+	},
+	{
+		icon: vueBootstrap,
+		name: "Vue-bootstrap",
+		color: "",
+		delay: 1400,
+		sDelay: "1s",
+		top: "210px",
+		left: "130px",
+	},
+
+	// third column
+	{
+		icon: django,
+		name: "DJANGO",
+		color: "#f0db4f",
+		delay: 1600,
+		sDelay: "1.2s",
+		top: "10px",
+		left: "260px",
+	},
+	{
+		icon: flask,
+		name: "FLASK",
+		color: "#f0db4f",
+		delay: 1800,
+		sDelay: "1.4s",
+		top: "110px",
+		left: "260px",
+	},
+	{
+		icon: git,
+		name: "GIT",
+		color: "#f0db4f",
+		delay: 2000,
+		sDelay: "1.6s",
+		top: "210px",
+		left: "260px",
+	},
+
+	// forth column
+	{
+		icon: database,
+		name: "DATABASE",
+		color: "#f0db4f",
+		delay: 2200,
+		sDelay: "1.8s",
+		top: "10px",
+		left: "390px",
+	},
+	{
+		icon: flutter,
+		name: "FLUTTER",
+		color: "#f0db4f",
+		delay: 2400,
+		sDelay: "2s",
+		top: "110px",
+		left: "390px",
+	},
+	{
+		icon: dart,
+		name: "DART",
+		color: "#f0db4f",
+		delay: 2600,
+		sDelay: "2.2s",
+		top: "210px",
+		left: "390px",
+	},
+
+	// fifth column
+	{
+		icon: html,
+		name: "HTML",
+		color: "#f0db4f",
+		delay: 2800,
+		sDelay: "2.4s",
+		top: "10px",
+		left: "510px",
+	},
+	{
+		icon: react,
+		name: "REACT.JS",
+		color: "#f0db4f",
+		delay: 3000,
+		sDelay: "2.6s",
+		top: "110px",
+		left: "510px",
+	},
+	{
+		icon: vue,
+		name: "VUE.JS",
+		color: "#f0db4f",
+		delay: 3200,
+		sDelay: "2.8s",
+		top: "210px",
+		left: "510px",
+	},
+	// sixth column
+	{
+		icon: python,
+		name: "PYTHON",
+		color: "#f0db4f",
+		delay: 3000,
+		sDelay: "3s",
+		top: "10px",
+		left: "640px",
+	},
+	{
+		icon: css,
+		name: "CSS",
+		color: "#f0db4f",
+		delay: 3200,
+		sDelay: "3.2s",
+		top: "110px",
+		left: "640px",
+	},
+	{
+		icon: bootstrap,
+		name: "BOOTSTRAP",
+		color: "#f0db4f",
+		delay: 3400,
+		sDelay: "3.4s",
+		top: "210px",
+		left: "640px",
+	},
+];
+
+export const DivGrow = styled("div")({
+	"@keyframes grow": {
+		"0%": {
+			width: "10px",
+			top: "0",
+		},
+		"50%": {
+			width: "15px",
+			top: "0",
+		},
+		"100%": {
+			width: "60px",
+			top: "40",
+		},
+	},
+	animation: "grow 4s 1",
+	position: "absolute",
+	width: "60px",
+	height: "60px",
+	// display:"flex",
+	// flexDirection:"column",
+	// alignItems:"center"
+});
