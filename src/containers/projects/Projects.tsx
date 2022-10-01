89d5acf5aa212777368d98d2692b4cbf9a09c498
murtaja1 <murtajaadnan7@gmail.com>
2022-10-01 10:51:05 +0300

    Build: add title to projects

--- src/containers/projects/Projects.tsx
+++ src/containers/projects/Projects.tsx
@@ -1,17 +1,20 @@
-import { Container, Grid, useMediaQuery } from "@mui/material";
+import { Container, Grid, Typography, useMediaQuery } from "@mui/material";
 import ProjectCard from "../../components/project/ProjectCard";
 
 const h = ["React.js", "JavaScript", "JavaScript", "JavaScript", "JavaScript"];
-const links = ["Repo", "Preview"];
 
 function Projects() {
 	const mobile = useMediaQuery("(min-width:600px)");
 
 	return (
-		<Container maxWidth='xl'
+		<Container
+			maxWidth="xl"
 			sx={{ mt: mobile ? "100px" : "10px", mb: mobile ? "10px" : "100px" }}
 		>
 			<Grid container justifyContent="center" alignItems="center" spacing={3}>
+				<Grid item xs={9.8}>
+					<Typography variant="h2">Projects:</Typography>
+				</Grid>
 				{h.map((_, index) => (
 					<Grid key={index} item>
 						<ProjectCard />
